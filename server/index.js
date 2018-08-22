8aafb4e02ecf82d726bc49efc403b65b86bc81ba
emrickgarrett <emrickgj@miamioh.edu>
2018-08-21 18:41:40 -0600

    Starting Google OAuth

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const app = express();
+require("./services/passport");
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
-});
+
+const app = express();
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
