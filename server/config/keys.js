1ab13cf260f483104e1d8cb4980f3b83765c7aa6
emrickgarrett <emrickgj@miamioh.edu>
2018-09-14 05:25:31 -0600

    Finished Auth flow

---
+++ server/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+	googleClientID: '1033214516485-26aiel7amo4skuti4i0sh3bhvhf00t8h.apps.googleusercontent.com',
+	googleClientSecret: 'Glc0QiINC2M59-uStJvNI4bP',
+	mongoRoute: 'mongodb://emrickgj:Mozilla!160@ds155252.mlab.com:55252/emaily-dev-emrickgj',
+	cookieKey: 'asdfkj15h15bgnmaqlal;fasdjf=0-11j23kda;dskffjncv145m,'
+}
