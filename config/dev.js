331a25f49b5520d423896f701615ca0a922dcca1
emrickgarrett <emrickgj@miamioh.edu>
2018-10-02 15:35:35 -0600

    oops

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	googleClientID: '1033214516485-26aiel7amo4skuti4i0sh3bhvhf00t8h.apps.googleusercontent.com',
-	googleClientSecret: 'Glc0QiINC2M59-uStJvNI4bP',
-	mongoRoute: 'mongodb://emrickgj:Mozilla!160@ds155252.mlab.com:55252/emaily-dev-emrickgj',
-	cookieKey: 'asdfkj15h15bgnmaqlal;fasdjf=0-11j23kda;dskffjncv145m,',
-	stripePublishableKey: 'pk_test_GJ1ueC0OWA0X7K8wWKKDhRXB',
-	stripeSecretKey: 'sk_test_2uBFaSGfEGDfbvZd7Aqw7FcA'
-}
