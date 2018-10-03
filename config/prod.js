81f52b1a92dc4f6ecce5b2cdef9ed3c8ba6fcb28
emrickgarrett <emrickgj@miamioh.edu>
2018-10-02 20:26:57 -0600

    Update

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,6 @@ module.exports = {
 	mongoRoute: process.env.MONGO_ROUTE,
 	cookieKey: process.env.COOKIE_KEY,
 	stripePublishableKey: process.env.STRIP_PUBLISHABLE_KEY,
-	stripeSecretKey: process.env.STRIPE_SECRET_KEY
+	stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+	sendGridKey: process.env.SEND_GRID_KEY
 }
